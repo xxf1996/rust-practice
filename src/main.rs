2c0b2316dc6ea991b36aa0a3ff4a3bf99415e9b9
xiexuefeng <1158231926@qq.com>
2021-09-07 19:05:22 +0800

    feat: 测试结构体

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
+#[derive(Debug)]
 struct Test {
-    user: String,
-    age: u8,
-    name: Option<String>,
-  }
-  
-  impl Test {
-    fn say(&self) {
-      println!("hello my name is {}", if self.name.is_some() { self.name });
-    }
-  }
+  user: String,
+  age: u8,
+  name: Option<String>,
+}
 
-fn borrow(str: &mut String) { // 可变引用，中途可以修改引用的值
-    println!("{}", str);
-    str.push_str("123");
+impl Test {
+  fn say(&self) {
+    println!("hello my name is {}", self.user);
+  }
+  fn test() {
+    println!("这不就是静态方法么");
+  }
+}
+
+fn borrow(str: &mut String) {
+  // 可变引用，中途可以修改引用的值
+  println!("{}", str);
+  str.push_str("123");
 }
 
 // fn borrow2(str: &mut String) {
@@ -22,13 +27,24 @@ fn borrow(str: &mut String) { // 可变引用，中途可以修改引用的值
 // }
 
 fn slice_test() {
-    let mut test = String::from("Adssfsfdsf");
-    let t1 = &mut test[1..3]; // 虽然String类型的slice可以设置成可变引用，但是引用结果为&str类型，str为字符字面量，无法修改；
-    println!("{}", t1);
-    // t1.clear();
+  let mut test = String::from("Adssfsfdsf");
+  let t1 = &mut test[1..3]; // 虽然String类型的slice可以设置成可变引用，但是引用结果为&str类型，str为字符字面量，无法修改；
+  println!("{}", t1);
+  // t1.clear();
+}
+
+fn struct_test() {
+  let s1 = Test {
+    user: String::from("sfdff"),
+    age: 123,
+    name: None,
+  };
+  s1.say();
+  println!("{:?}", s1);
+  println!("{:#?}", s1);
+  Test::test();
 }
 
 fn main() {
-    slice_test();
+  struct_test();
 }
-
